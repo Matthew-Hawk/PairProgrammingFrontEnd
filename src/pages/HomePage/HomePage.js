2a32735d147af6dbe6a38a2cc124bd80b41e444c
Chris Lee <dkrj45@gmail.com>
2022-09-01 12:43:42 -0400

    all axios functions added. only requires styling

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,16 @@
 import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function HomePage() {
 
+    const [randomImage, setRandomImage] = useState(null)
+
+    let num = Math.floor(Math.random()*18)
+
     const handleOnClick = () => {
-        axios.get("http://localhost:8080/images").then(res => {
-            console.log(res.data)
+        axios.get("http://localhost:8080/images").then(res =>{
+            setRandomImage(res.data[num].image)
+            console.log(num)
         })
     }
 
@@ -14,8 +20,8 @@ function HomePage() {
             <h1 className='home-page__title'>A1-Image</h1>
             <h2 className='home-page__sub-text'> Click the button to generate some awesome AI art</h2>
             <button onClick={handleOnClick} className="home-page__button">get images</button>
-            <div className=''>
-                //insert the generated photo here
+            <div className='home-page__image'>
+                <img src={randomImage} />
             </div>
         </div>
         </>
